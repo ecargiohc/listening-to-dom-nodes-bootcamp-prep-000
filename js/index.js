b838b2d10de406216e88bb660919944d0a052e67
gc6123 <gc6123@bard.edu>
2018-10-10 00:32:11 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ main.addEventListener('click', function(event) {
 let divs = document.querySelectorAll('div');
  
 function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
