0556724bb103d4948900b6b0a238b141813787da
gc6123 <gc6123@bard.edu>
2018-10-10 00:32:02 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -3,3 +3,6 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
